df96837a42d9848570a02274ae382d5f1b1118e4
iraklikeche <irakli.kechee@gmail.com>
2023-08-22 13:57:24 +0400

    SideNavigation Component

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import SideNavigation from "./components/SideNavigation.vue";
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SideNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
