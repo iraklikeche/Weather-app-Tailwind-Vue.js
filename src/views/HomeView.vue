d5ec71f5036a91c71aa109abb58d8fa44865439c
iraklikeche <irakli.kechee@gmail.com>
2023-08-23 12:03:34 +0400

    fetching API with axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,51 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        v-model="searchQuery"
+        @input="getSearchResults"
+        type="text"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResults in mapboxSearchResults"
+          :key="searchResults.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResults.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey =
+  "pk.eyJ1Ijoia2VjaGUiLCJhIjoiY2xsbmYzM2VyMDA1NzNlbzRmczJyMTU3NyJ9.ucAR0xxEzW9IHdO4Dwsgmg";
+const searchQuery = ref("");
+const queryTimeOut = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeOut.value);
+  queryTimeOut.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+      );
+      mapboxSearchResults.value = result.data.features;
+      console.log(mapboxSearchResults.value);
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
