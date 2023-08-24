7a6b8ed57b97562e0a9df1b7c0ab85a25b12edae
iraklikeche <irakli.kechee@gmail.com>
2023-08-24 21:31:08 +0400

    Error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,13 +12,19 @@
         v-if="mapboxSearchResults"
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
       >
-        <li
-          v-for="searchResults in mapboxSearchResults"
-          :key="searchResults.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ searchResults.place_name }}
-        </li>
+        <p v-if="searchError">Sorry, something went wrong, please try.</p>
+        <p v-if="mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResults in mapboxSearchResults"
+            :key="searchResults.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ searchResults.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -33,16 +39,21 @@ const mapboxAPIKey =
 const searchQuery = ref("");
 const queryTimeOut = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeOut.value);
   queryTimeOut.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value);
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value);
+      } catch {
+        searchError.value = true;
+      }
       return;
     }
     mapboxSearchResults.value = null;
