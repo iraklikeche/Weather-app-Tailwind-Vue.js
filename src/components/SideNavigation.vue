4391c7405d22da4409b0d4a24d0cb01d71f2eecf
iraklikeche <irakli.kechee@gmail.com>
2023-08-29 11:12:59 +0400

    Add city to LocalStorage.

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -17,6 +17,8 @@
         ></i>
         <i
           class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="addCity"
+          v-if="route.query.preview"
         ></i>
       </div>
       <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
@@ -54,13 +56,39 @@
 </template>
 
 <script setup>
-import { RouterLink } from "vue-router";
+import { RouterLink, useRoute, useRouter } from "vue-router";
 import BaseModal from "./BaseModal.vue";
 import { ref } from "vue";
+import { uid } from "uid";
 
 const modalActive = ref(null);
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
 const toggleModal = () => {
   modalActive.value = !modalActive.value;
+};
+
+const addCity = () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value === JSON.parse(localStorage.getItem("savedCities"));
+  }
+  const locationObj = {
+    id: uid(),
+    state: route.params.state,
+    city: route.params.city,
+    coords: {
+      lat: route.query.lat,
+      lng: route.query.lng,
+    },
+  };
+
+  savedCities.value.push(locationObj);
+  localStorage.setItem("savedCities", JSON.stringify(savedCities.value));
+
+  let query = Object.assign({}, route.query);
+  delete query.preview;
+  router.replace({ query });
 };
 </script>
 
